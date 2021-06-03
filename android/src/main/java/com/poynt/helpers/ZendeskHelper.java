47c141917527ebfbe97ddd24dd5b9e6d0879a7d1
tarnautova-godaddy <tarnautova@godaddy.com>
2021-06-03 17:53:19 +0300

    Fix

--- android/src/main/java/com/poynt/helpers/ZendeskHelper.java
+++ android/src/main/java/com/poynt/helpers/ZendeskHelper.java
@@ -13,6 +13,11 @@ import java.util.List;
 public class ZendeskHelper implements ReactPackage {
 
     @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
     public List<NativeModule> createNativeModules(
             ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
